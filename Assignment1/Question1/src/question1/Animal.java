be31f9dd1bf79f3e36dddb112b0ebf2ffb32e77e
mushruf12 <mushrufs@gmail.com>
2022-09-28 22:09:38 +0530

    commit

--- Assignment1/Question1/src/question1/Animal.java
+++
@@ -1,8 +0,0 @@
-package question1;
-
-public abstract class Animal {
-	abstract void walk();
-	abstract void type();
-	abstract void fly();
-
-}
--- Assignment1/Question2/src/question2/Mobile.java
+++
@@ -1,8 +0,0 @@
-package question2;
-//Abstract Class
-
-public abstract class Mobile {
-	abstract String os();
-	abstract int version(); 
-
-}
